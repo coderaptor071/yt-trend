a6c15f099c2048a462e876337cb1ef397d7e878b
Simarpal Singh <simarsingh071@google.com>
2024-05-03 13:41:18 +0530

    tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  important: true,
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./Components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
